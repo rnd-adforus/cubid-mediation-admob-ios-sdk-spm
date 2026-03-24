f72e777107bb8fa55cdef651574f712ca9187976
SungEunAdforus <jennet@adforus.co.kr>
2026-03-24 13:11:46 +0900

    Update README.md
    Update Package.swift
    
    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "UAdKit",
-            targets: ["UAdKit"]
+            targets: ["UAdKitTarget"]
         ),
     ],
     dependencies: [
